ee609d1101db5624069ea2e98418a41ce84adfdc
juliet879 <gisembajuliett@gmail.com>
2021-03-21 21:47:35 +0300

    created the scripts file

---
+++ script.js
@@ -0,0 +1,12 @@
+var fruit = {
+    name : "Pineaple",
+    price : 30,
+    quantity : 2,
+    total : function(price,quantity){
+        cost = price * quantity
+        return this.total = cost
+    }
+}
+    fruit.total(30,2)
+console.log(fruit.quantity + " " + fruit.name + "for KSH " + cost.toFixed(2))
+
